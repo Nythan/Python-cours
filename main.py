f1c96f483971630aa9ad763d82f2b69927716d2c
Nythan <julien.damette@outlook.fr>
2023-01-18 11:26:46 +0100

    Cours python partie 1

---
+++ main.py
@@ -0,0 +1,7 @@
+nb = int(input("saisir un entier :"))
+#chnb= input("saisir un entier :")
+#nb=int(chnb)
+if nb%2 == 0:
+    print(nb,  "est un nombre pair")
+else:
+    print(nb, "est un nombre impair")
