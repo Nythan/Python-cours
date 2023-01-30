0b2e2806bb94b18a45760581afdef248a1662e68
Nythan <71347666+Nythan@users.noreply.github.com>
2023-01-30 12:03:53 +0100

    Update ex1_8.py
    Édit pour les attente des profs

--- ex1_8.py
+++ ex1_8.py
@@ -1,11 +1,11 @@
 l = [0,0,0,0]
 nbCas = 0
-for i in range(0,10000):
+for i in range(1000,9999):
     l[0] = int(i/1000)
     l[1] = int(i%1000/100)
     l[2] = int(i%100/10)
     l[3] = int(i%10)
-    if l[0]*10+l[1]+l[2]*10+l[3] == l[1]*10+l[2]:
+    if l[0]*10+l[1]+l[2]*10+l[3] == l[1]*10+l[2] and l[0]!=0:
         nbCas = nbCas+1
         print(l)
 print(nbCas)
